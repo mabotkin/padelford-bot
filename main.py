8b5486cea52a45daa9b6f983521b259ccd47424c
Alex Wang <alex3763@gmail.com>
2022-07-19 20:44:31 -0400

    that is a good point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import random
 import os
 
 bot = discord.Bot()
@@ -6,6 +7,10 @@ bot = discord.Bot()
 GUILD_IDS = [
 	int( os.environ[ "GUILD_ID" ] )
 ]
+
+USER_IDS = {
+	"Sean" : 741470146706800660
+}
 
 @bot.event
 async def on_ready():
@@ -15,6 +20,10 @@ async def on_ready():
 async def on_message( message ):
 	if message.author == bot.user:
 		return
+	if message.author.id == USER_IDS[ "Sean" ] :
+		if random.random() < 0.05:
+			await message.reply( "That is a good point!" )
+
 
 @bot.slash_command( name = "about" , description = "Shows an About Me message." , guild_ids = GUILD_IDS )
 async def about( ctx ):
