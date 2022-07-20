29a05cd360a1d1ad6a72ee1b3ab6bf97ee840218
tchin <tracy.chin42@gmail.com>
2022-07-19 22:40:06 -0700

    Update main.py to use modern Bot class instead of Client class.

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ from dotenv import load_dotenv
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client( intents = intents )
+# client = discord.Client( intents = intents )
+bot = discord.Bot(debug_guilds = [997949327974604911])
 
-@client.event
+@bot.event
 async def on_ready():
-	print( f'We have logged in as {client.user}' )
+	print( f'We have logged in as {bot.user}' )
 
-@client.event
-async def on_message( message ):
-	if message.author == client.user:
-		return
-
-	if message.content.lower() == '!bot' :
-		await message.channel.send( 'Hello! I am a friendly bot here to help facilitate activities on the In-Came Autumn 2021 Math Grad Students Discord Server.  Any resemblance to Jackson Morris is purely coincidental.' )
+@bot.slash_command(name = "bot")
+async def bot_command(ctx):
+	await ctx.respond( 'Hello! I am a friendly bot here to help facilitate activities on the In-Came Autumn 2021 Math Grad Students Discord Server.  Any resemblance to Jackson Morris is purely coincidental.' )
 
 key = ""
 if "API_KEY" in dict(os.environ):
@@ -26,4 +23,4 @@ else:
 	load_dotenv()
 	key = os.getenv("API_KEY")
 
-client.run( key )
+bot.run( key )
