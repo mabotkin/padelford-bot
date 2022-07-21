68c9fc39b8fa0efa0e984fe9640e14ed37a2b37d
Alex Wang <alex3763@gmail.com>
2022-07-20 21:16:11 -0400

    google sheets integration

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discord
 import random
 import os
 from dotenv import load_dotenv
+import gspread
+import json
+from oauth2client.service_account import ServiceAccountCredentials
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,8 +12,18 @@ intents.message_content = True
 load_dotenv()
 bot = discord.Bot(intents = intents)
 
+# connect gsheets
+scopes = [ 'https://spreadsheets.google.com/feeds' ]
+json_creds = os.getenv( "GOOGLE_SHEETS_CREDS_JSON" )
+
+creds_dict = json.loads( json_creds , strict = False )
+creds_dict[ "private_key" ] = creds_dict[ "private_key" ].replace( "\\\\n" , "\n" )
+creds = ServiceAccountCredentials.from_json_keyfile_dict( creds_dict , scopes )
+client = gspread.authorize( creds )
+spreadsheet = client.open_by_url( os.getenv( "SPREADSHEET_URL" ) )
+
 GUILD_IDS = [
-	int( os.environ[ "GUILD_ID" ] )
+	int( os.getenv( "GUILD_ID" ) )
 ]
 
 USER_IDS = {
@@ -37,7 +50,7 @@ async def on_message( message ):
 async def about( ctx ):
 	await ctx.respond( 'Hello! I am a friendly bot here to help facilitate activities on the In-Came Autumn 2021 Math Grad Students Discord Server.  Any resemblance to Jackson Morris is purely coincidental.' )
 
-@bot.command( name = "add_role", description = "Give yourself a fun new identity.", guild_ids = GUILD_IDS)
+@bot.slash_command( name = "add_role", description = "Give yourself a fun new identity.", guild_ids = GUILD_IDS)
 async def add_role( ctx, *, role: discord.Role):
 	member = ctx.author
 	await member.add_roles(role)
@@ -49,5 +62,10 @@ async def interest_role( ctx ):
 	role = discord.utils.get( ctx.guild.roles , name = "math" )
 	await member.add_roles( role )
 	await ctx.respond( f"Gave math role to { member.mention }." )
+
+@bot.slash_command( name = "faq" , description = "Displays FAQ messages." , guild_ids = GUILD_IDS )
+async def faq( ctx ):
+	sheet = spreadsheet.worksheet( "FAQ" )
+	await ctx.respond( str( sheet.get_all_records() ) )
 
 bot.run( os.getenv("API_KEY") )
